8fc5e80fe3669cdb395efd2c951d5ad05c99cb37
Sam Gallacher <sgcsamdev@outlook.com>
2024-03-17 19:46:50 +0000

    Core - added build support to COLR8 section.

--- XUIHelper.Core/XUIHelper.Core/XUR/V8/Sections/COLR8Section.cs
+++ XUIHelper.Core/XUIHelper.Core/XUR/V8/Sections/COLR8Section.cs
@@ -59,7 +59,164 @@ namespace XUIHelper.Core
 
         public async Task<bool> TryBuildAsync(IXUR xur, XUObject xuObject)
         {
-            throw new NotImplementedException();
+            try
+            {
+                xur.Logger?.Here().Verbose("Building COLR8 colours.");
+                HashSet<XUColour> builtColours = new HashSet<XUColour>();
+                if (!TryBuildColoursFromObject(xur, xuObject, ref builtColours))
+                {
+                    xur.Logger?.Here().Error("Failed to build colours, returning null.");
+                    return false;
+                }
+
+                Colours = builtColours.ToList();
+                xur.Logger?.Here().Verbose("Built a total of {0} COLR8 colours successfully!", Colours.Count);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                xur.Logger?.Here().Error("Caught an exception when trying to build COLR8 colours, returning false. The exception is: {0}", ex);
+                return false;
+            }
+        }
+
+        private bool TryBuildColoursFromObject(IXUR xur, XUObject xuObject, ref HashSet<XUColour> builtColours)
+        {
+            try
+            {
+                if (!TryBuildColoursFromProperties(xur, xuObject.Properties, ref builtColours))
+                {
+                    xur.Logger?.Here().Error("Failed to build colours from properties for {0}, returning false.", xuObject.ClassName);
+                    return false;
+                }
+
+                foreach (XUObject childObject in xuObject.Children)
+                {
+                    if (!TryBuildColoursFromObject(xur, childObject, ref builtColours))
+                    {
+                        xur.Logger?.Here().Error("Failed to get colours for child {0}, returning false.", childObject.ClassName);
+                        return false;
+                    }
+                }
+
+                foreach (XUTimeline childTimeline in xuObject.Timelines)
+                {
+                    foreach (XUKeyframe childKeyframe in childTimeline.Keyframes)
+                    {
+                        foreach (XUProperty animatedProperty in childKeyframe.Properties)
+                        {
+                            if (animatedProperty.PropertyDefinition.Type == XUPropertyDefinitionTypes.Colour)
+                            {
+                                if(animatedProperty.PropertyDefinition.FlagsSet.Contains(XUPropertyDefinitionFlags.Indexed))
+                                {
+                                    int valueIndex = 0;
+                                    foreach (object? valueObj in animatedProperty.Value as List<object?>)
+                                    {
+                                        if(animatedProperty.Value == null)
+                                        {
+                                            //This index isn't animated
+                                            continue;
+                                        }
+
+                                        if (valueObj is not XUColour valueColour)
+                                        {
+                                            xur.Logger?.Here().Error("Animated indexed child property {0} at index {1} marked as colour had a non-colour value of {2}, returning false.", animatedProperty.PropertyDefinition.Name, valueIndex, valueObj);
+                                            return false;
+                                        }
+
+                                        if (builtColours.Add(valueColour))
+                                        {
+                                            xur.Logger?.Here().Verbose("Added {0} animated indexed property value index {1} colour {2}.", animatedProperty.PropertyDefinition.Name, valueIndex, valueColour);
+                                        }
+
+                                        valueIndex++;
+                                    }
+                                }
+                                else
+                                {
+                                    if (animatedProperty.Value is not XUColour valueColour)
+                                    {
+                                        xur.Logger?.Here().Error("Animated property {0} marked as colour had a non-colour value of {1}, returning false.", animatedProperty.PropertyDefinition.Name, animatedProperty.Value);
+                                        return false;
+                                    }
+
+                                    if (builtColours.Add(valueColour))
+                                    {
+                                        xur.Logger?.Here().Verbose("Added {0} animated property value colour {1}.", animatedProperty.PropertyDefinition.Name, valueColour);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                xur.Logger?.Here().Error("Caught an exception when trying to build COLR8 colours for object {0}, returning false. The exception is: {1}", xuObject.ClassName, ex);
+                return false;
+            }
+        }
+
+        private bool TryBuildColoursFromProperties(IXUR xur, List<XUProperty> properties, ref HashSet<XUColour> builtColours)
+        {
+            try
+            {
+                foreach (XUProperty childProperty in properties)
+                {
+                    if (childProperty.PropertyDefinition.Type == XUPropertyDefinitionTypes.Colour)
+                    {
+                        if (childProperty.PropertyDefinition.FlagsSet.Contains(XUPropertyDefinitionFlags.Indexed))
+                        {
+                            int valueIndex = 0;
+                            foreach (object valueObj in childProperty.Value as List<object>)
+                            {
+                                if (valueObj is not XUColour valueColour)
+                                {
+                                    xur.Logger?.Here().Error("Indexed child property {0} at index {1} marked as colour had a non-colour value of {2}, returning false.", childProperty.PropertyDefinition.Name, valueIndex, valueObj);
+                                    return false;
+                                }
+
+                                if (builtColours.Add(valueColour))
+                                {
+                                    xur.Logger?.Here().Verbose("Added {0} indexed property value index {1} colour {2}.", childProperty.PropertyDefinition.Name, valueIndex, valueColour);
+                                }
+
+                                valueIndex++;
+                            }
+                        }
+                        else
+                        {
+                            if (childProperty.Value is not XUColour valueColour)
+                            {
+                                xur.Logger?.Here().Error("Child property {0} marked as colour had a non-colour value of {1}, returning false.", childProperty.PropertyDefinition.Name, childProperty.Value);
+                                return false;
+                            }
+
+                            if (builtColours.Add(valueColour))
+                            {
+                                xur.Logger?.Here().Verbose("Added {0} property value colour {1}.", childProperty.PropertyDefinition.Name, valueColour);
+                            }
+                        }
+                    }
+                    else if (childProperty.PropertyDefinition.Type == XUPropertyDefinitionTypes.Object)
+                    {
+                        if (!TryBuildColoursFromProperties(xur, childProperty.Value as List<XUProperty>, ref builtColours))
+                        {
+                            xur.Logger?.Here().Error("Failed to build colours for child compound properties, returning false.");
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                xur.Logger?.Here().Error("Caught an exception when trying to build COLR8 colours from properties, returning false. The exception is: {0}", ex);
+                return false;
+            }
         }
 
         public async Task<int?> TryWriteAsync(IXUR xur, XUObject xuObject, BinaryWriter writer)
--- XUIHelper.Core/XUIHelper.Tests/XUR8Tests.cs
+++ XUIHelper.Core/XUIHelper.Tests/XUR8Tests.cs
@@ -8,6 +8,7 @@ namespace XUIHelper.Tests
     {
         //TODO: XUR8 write support
         //TODO: XUR8 unit tests
+        //TODO: Genericise the build floats, quats, vectors, etc into a templated function
         //TODO: Decouple XUI version from extensions, make it more of a group IDed by a string, as we should be able to write a XUR8 as a XUR5
         //TODO: Support for ignore properties (ones that aren't supported in XuiTool)
         //TODO: Function library API
